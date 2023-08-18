abb6a0ee232dc08f05cc1b0b8fc5b5a2d39ef7c9
mrowehill <141061772+mrowehill@users.noreply.github.com>
2023-08-18 11:34:41 -0500

    scripts.js new pokemonList array
    added 3 pokemon objects to array with 4 keys each

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 let pokemonList= [];
+pokemonList = [
+    {
+        name: 'Chikorita',
+        height: .889,
+        type: ['Grass'],
+        weakness: ['Fire', 'Flying', 'Ice', 'Poison', 'Bug']
+    },
+    {
+        name: 'Cyndaquil',
+        height: .508,
+        type: ['Fire'],
+        weakness: ['Water', 'Ground', 'Rock']
+    },
+    {
+        name: 'Totodile',
+        height: .609,
+        type: ['Water'],
+        weakness: ['Grass', 'Electric']
+    }
+]
