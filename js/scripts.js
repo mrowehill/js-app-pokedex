82d2246924dd5d617020c69359940cf8bf92f9a4
mrowehill <141061772+mrowehill@users.noreply.github.com>
2023-08-21 15:50:08 -0500

    updated pokemonList loop to if-else
    made the pokemon loop to an if-else loop

--- js/scripts.js
+++ js/scripts.js
@@ -20,5 +20,14 @@ let pokemonList = [
 ];
 
 for (let i=0; i<pokemonList.length; i++) {
-    document.write(pokemonList[i].name + pokemonList[i].height);
+    if (pokemonList[i].height > 0.7){
+        document.write(
+            pokemonList[i].name + ' (Height:' + pokemonList[i].height +')' + '-That\'s a big pokemon!'
+        ); //Writes name and height of a pokemon that exceeds 0.7 in height and declares it a big pokemon
+    } else {
+        document.write(
+            pokemonList[i].name + ' (Height:' + pokemonList[i].height +')' 
+        ) //Writes just the name and height of a pokemon that does not exceed a height of 0.7
+    }
+    document.write('<br> <br>')
 }
